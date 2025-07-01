16dbf1b41fd94a553918fba83152043580ce20e5
jogalvisa <jogalvisa@unal.edu.co>
2025-07-01 10:37:02 -0500

    corrección 13

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -44,9 +44,12 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
 
-SELECT 
-    K0, 
-    ROUND(AVG(c12), 2) AS "avg(c12)"
+SELECT
+  K0,
+  -- multiplica por 100, añade 0.5, trunca y divide para lograr
+  -- un redondeo “hacia arriba” en caso de .005 exactos
+  (CAST(AVG(c12) * 100.0 + 0.5 AS INTEGER)) / 100.0 AS "avg(c12)"
 FROM tbl1
-WHERE c13 > 400
-GROUP BY K0;
+WHERE c13 >= 500
+GROUP BY K0
+ORDER BY K0;
